dc78685e5813a308f04c4f432fe8e3924aef07fa
TeejMcSteez <tjhall047@gmail.com>
2025-02-26 18:43:39 -0500

    Added some more frontend stuff and a new blog page

--- client/src/routes/blog/+page.svelte
+++ client/src/routes/blog/+page.svelte
@@ -1,8 +1,31 @@
+<script>
+
+   import { goto } from "$app/navigation";
+
+   async function getBlog() {
+      const res = await fetch('http://localhost:3000/blog');
+      const data = await res.json();
+      console.log(data);
+   }
+</script>
 <!-- TODO:
- 1. Add a landing page for block posts
-    1a. The landing page will display the title of the most recent posts in a scrolling manner. 
- 2. Upon clicking on a title, the page will re-render with the fetched contents (in markdown format) of the post.
-    2a. With the title as the header isntead of the blog landing page and the contents of the post below it.
-    2b. Also try to make a smoothe and clean in possbile in transition but never did this before so might look like shit lol.
- 3. Add a section for comments
+ Main: Figure out a smart way to actually get blogs and post them to the landing page and then find a sensible way for the user 
+ to the view the blog which will be in markdown with either loading a new window or re-rendering the page somehow.
+
+ 1. Upon clicking on a title, the page will re-render with the fetched contents (in markdown format) of the post.
+    1a. With the title as the header isntead of the blog landing page and the contents of the post below it.
+    1b. Also try to make a smoothe and clean in possbile in transition but never did this before so might look like shit lol.
+ 2. Add a section for comments
+ 3. add a function on:click will fetch the blog title in the tag from the node server.
 -->
+
+<div class="bg-zinc-600 min-w-screen min-h-screen">
+   <div id="header" class="bg-zinc-800 flex flex-row justify-between items-center min-w-screen p-12">
+      <h1 class="text-4xl text-white">Blog Posts</h1>
+      <button on:click={async () => await goto('/')} class="text-3xl bg-zinc-900 text-white hover:bg-red-800 hover:animate-pulse rounded-xl p-3">Home</button>
+   </div>
+   
+   <div id="blogPostSection" class="m-5 p-10 text-2xl text-white flex flex-col items-center justify-center">
+      <h1>Figure out a smart way to actually get blogs and post them to the landing page and then find a sensible way for the user to the view the blog which will be in markdown with either loading a new window or re-rendering the page somehow</h1>
+   </div>
+</div>
--- client/src/routes/contact/+page.svelte
+++ client/src/routes/contact/+page.svelte
@@ -7,6 +7,8 @@
         <a id = "button" class="w-24 text-center bg-zinc-900 hover:bg-red-600 hover:animate-pulse p-2 m-2 rounded-xl text-white" href="https://www.threads.net/@tj_hall04" target="_blank">Threads</a>
         <a id="button" class="w-24 text-center bg-zinc-900 hover:bg-red-600 hover:animate-pulse p-2 m-2 rounded-xl text-white" href="https://www.linkedin.com/in/tommyhall047/" target="_blank">LinkedIn</a>
         <a id="button" class="w-24 text-center bg-zinc-900 hover:bg-red-600 hover:animate-pulse p-2 m-2 rounded-xl text-white" href="https://profile.indeed.com/p/tommyh-m1cgdb4" target="_blank">Indeed</a>
+        <a id="button" class="w-24 text-center bg-zinc-900 hover:bg-red-600 hover:animate-pulse p-2 m-2 rounded-xl text-white" href="/blog" target="_blank">Blog</a>
+
         <a id = "backbutton" class="w-24 text-center bg-zinc-900 hover:bg-red-600 hover:animate-pulse p-2 m-2 rounded-xl text-white" href="/"><button>Back</button></a>
     </div>
 </div>

6796e7a6cc961f4215296272095869ae213abccf
TeejMcSteez <tjhall047@gmail.com>
2025-02-26 01:13:37 -0500

    Added more frontend stuff and a custom err page that I personally love

---
+++ client/src/routes/+error.svelte
@@ -0,0 +1,5 @@
+<script>
+    import err from '../lib/assets/err.jpg';
+</script>
+<img src={err} alt="Error Dinosaur" class="m-w-screen m-h-screen">
+<h1 class="text-3xl flex-col text-center">Ah ah ah, that wasn't the magic word . . . </h1>
--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,11 +1,12 @@
 <script>
     
     import { onMount } from 'svelte';
-    import { animate } from 'motion';
+    import { animate, time } from 'motion';
     // Photos
     import distanceDoorSesnsor from '$lib/assets/distanceDoorSesnsor.png';
     import exampleSRSM from '$lib/assets/exampleSRSM.png';
     import loginSucc from '$lib/assets/loginSuccesfful.png';
+    import { once } from 'svelte/legacy';
     
     /** @type {HTMLElement[]} */
     let sections = [];
@@ -16,6 +17,9 @@
                 entries.forEach(entry => {
                     if (entry.isIntersecting) {
                         animate(entry.target, { opacity: [0, 1], y: [50, 0] }, { duration: Math.random()}); // Random duration to make it feel more natural
+                        if (entry.target === sections[5]) {
+                            typeAbout();
+                        }
                         observer.unobserve(entry.target);
                     }
                 });
@@ -24,7 +28,40 @@
         );
         sections.forEach(section => observer.observe(section));
     });
-    
+
+    function scrollToTop() {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+
+    function typeAbout() {
+        const aboutChar = "About Me";
+        const timeout = 30;
+
+        let charArr = [];
+
+        for (let i = 0; i < aboutChar.length; i++) {
+            charArr.push(aboutChar.charAt(i));
+        }
+
+        const aboutMe = document.getElementById('aboutMeTag');
+
+        if (aboutMe?.innerHTML) {
+            aboutMe.innerHTML = "";
+        }
+
+        setTimeout(() => {
+            let i = 0;
+            let interval = setInterval(() => {
+                if (i < charArr.length && aboutMe) { // If about me is not null and the index is not greater than the length of the array will type
+                    aboutMe.innerHTML += charArr[i];
+                    i++;
+                } else {
+                    clearInterval(interval);
+                }
+            }, timeout);
+        }, 1000);       
+    }
+
 </script>
 
 <div id="body" class="bg-zinc-800 overflow-auto">
@@ -32,13 +69,13 @@
     <div id="headerSection" class="bg-black flex flex-row justify-between items-center p-8">
         <h1 class="sm:text-xl text-3xl font-bold text-white">Tommy Halls Website</h1>
         <nav>
-            <a href="/contact" class="text-white hover:bg-slate-800 hover:animate-pulse bg-red-800 border-red-800 rounded-xl p-2 m-3">Contact</a>
+            <a href="/contact" class="text-white hover:bg-slate-800 hover:animate-pulse bg-red-800 border-red-800 rounded-xl p-3 m-4">Contact</a>
         </nav>
     </div>
     
     <div id="line" class="m-px p-px bg-red-800 min-w-screen"></div>
     <!-- Figure out how I want sectioning, Depending on how I want to the website to feel -->
-    <section bind:this={sections[0]} style="opacity: 0;" class="m-5 p-2 flex flex-col items-center justify-center">
+    <section bind:this={sections[0]} style="opacity: 0;" class="min-w-screen min-h-screen m-5 p-2 flex flex-col items-center justify-center">
         <div id="container" class="m-4 p-2 bg-zinc-900 rounded-xl">
             <div id="intro" class="text-white p-4 m-4 text-center">
                 <h2 class="m-4 text-2xl">Hello my name is Tommy Hall!</h2>
@@ -47,7 +84,8 @@
             </div>
         </div>
     </section>
-    <section bind:this={sections[1]} style="opacity: 0;" class="m-5 p-2 flex flex-col items-center justify-center">
+
+    <section bind:this={sections[1]} style="opacity: 0;" class="min-w-screen min-h-screen m-5 p-2 flex flex-col items-center justify-center">
         <div id="pictureContainer">
             <!-- Pictures in scroll sections with the picture section being its own section -->
             <section bind:this={sections[2]} style="opacity: 0;" class="m-5 p-2 m-w-80 m-h-80">
@@ -60,12 +98,14 @@
             </section>
         </div>
     </section>
-    <section bind:this={sections[5]} class="flex flex-col items-center justify-center">
-        <div id="AboutMeHeader" class="m-px p-4 rounded-xl bg-zinc-900 text-white text-4xl">
-            <h1>About Me</h1>
+
+    <section bind:this={sections[5]} style="opacity: 0;" id="aboutSection" class="min-w-32 min-h-32 flex flex-col items-center justify-center">
+        <div id="AboutMeHeader" class="m-px p-4 rounded-xl bg-zinc-900 text-white text-4xl text-center">
+            <h1 id="aboutMeTag">Loading Typewriter . . .</h1>
         </div>
     </section>
-    <section bind:this={sections[6]} style="opacity: 0;" class="m-5 p-2 flex flex-col items-center justify-center">
+
+    <section bind:this={sections[6]} style="opacity: 0;" class="min-w-screen min-h-80 m-5 p-2 flex flex-col items-center justify-center">
         <div id="aboutMe" class="m-4 p-4 bg-zinc-900 rounded-xl text-white text-center text-xl">
             <h2>I am currently an undergraduate student at The University of North Carolina at Charlotte majoring in Computer Science</h2>
             <h2>I am undecided between software development and cybersecurity as my major, as  I feel web devs must know cybersecurity and researchers/cybersecurity specialist should know some programming</h2>
@@ -83,6 +123,8 @@
             <h2>Thank you for visiting my website!</h2>
             <h2>Feel free to check out my contact page for more information on how to reach me!</h2>
         </div>
+        <button onclick={scrollToTop} class="text-center bg-zinc-900 text-white p-4 m-4 rounded-xl justify-center items-center hover:bg-red-800 hover:animate-pulse">Scroll To Top</button>
     </section>
+
 
 </div>

2762641f72a2a5db3b90ebecc64e07da0315250c
TeejMcSteez <tjhall047@gmail.com>
2025-03-06 19:04:40 -0500

    Added speed insights from vercel

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,7 +1,10 @@
 <script>
 	import '../app.css';
-	import { dev } from '$app/environment'
-	import { injectAnalytics } from '@vercel/analytics/sveltekit'
+	import { dev } from '$app/environment';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
+	import { injectSpeedInsights } from '@vercel/speed-insights';
+
+	injectSpeedInsights();
 	injectAnalytics({ mode: dev ? 'development' : 'production' });
 	let { children } = $props();
 </script>

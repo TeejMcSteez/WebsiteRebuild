dcd16a85e9ad67415d6c52cbded12c5aec1e7d8d
TeejMcSteez <tjhall047@gmail.com>
2025-03-07 00:37:41 -0500

    Added basic Google OAuth and session storage with express-session and passport

--- server/index.js
+++ server/index.js
@@ -10,40 +10,43 @@ const { createClient } = require('@supabase/supabase-js');
 const jwt = require('jsonwebtoken');
 const { uid } = require('uid');
 
+const session = require('express-session');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const POSTS_DIR = path.join(__dirname, 'posts');
 
 const supabase = createClient('https://dzqcqtucdqznjvagxmhj.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR6cWNxdHVjZHF6bmp2YWd4bWhqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA4MDE5OTUsImV4cCI6MjA1NjM3Nzk5NX0.iUdqHG_dvoEfqndwfgqhz1ikik7H7PPk2okKlz8xlb8');
 
 app.use(cors());
 app.use(server.json());
+app.use(session({
+    secret: fs.readFileSync(path.join(__dirname, process.env.KEYPATH)),
+    resave: false,
+    saveUninitialized: true,
+}));
 
-function signToken() {
-    return jwt.sign({ id: uid()}, // random uid
-        fs.readFileSync(path.join(__dirname, process.env.KEYPATH)), // sign key
-        {
-            expiresIn: '1h', // expires in an hr
-        }
-    );
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Passport setup
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+passport.deserializeUser((obj, done) => {
+    done(null, obj);
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: '/auth/google/callback'
+},
+(accessToken, refreshToken, profile, done) => {
+    return done(null, profile);
 }
-
-// Authentication middleware
-const requireAuth = async (req, res, next) => {
-    // use jwt or local storage to properly authenticate the user
-    if (!res.cookie.token) {
-        const token = signToken();
-        res.cookie('token', token, {httpOnly: true});
-        res.redirect('/');
-    } else {
-        const verify = jwt.verify(res.cookie.token);
-
-        if (!verify) {
-            res.redirect('/login.html');
-        } else {
-            res.redirect('/');
-        }
-    }
-
-};
+));
 
 function getBlogTitles() {
     const blogTitles = fs.readdirSync(POSTS_DIR);
@@ -58,11 +61,20 @@ function getBlogPost(title) {
 
 // Routes
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'admin.html'));
+
+    if(req.isAuthenticated()) {
+        res.sendFile(path.join(__dirname, 'admin.html'));
+    } else {
+        res.sendFile(path.join(__dirname, 'login.html'));
+    }
+
 });
 // Add authing route handling for google login
-app.get('/api/google/auth', async (req, res) => {
-    
+app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+app.get('/auth/google/callback', passport.authenticate('google', {failureRedirect: '/login.html'}),
+(req, res) => {
+    res.redirect('/');
 });
 
 app.get('/api/blogTitles', (req, res) => {

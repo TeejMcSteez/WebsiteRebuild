913283eb66ae1a00a46144217903d7929449b8a9
TeejMcSteez <tjhall047@gmail.com>
2025-03-08 23:27:02 -0500

    Oops wrong file

--- client/src/routes/api/supabase/+server.js
+++ client/src/routes/api/supabase/+server.js
@@ -2,7 +2,9 @@ import { json } from '@sveltejs/kit';
 
 import { createClient } from '@supabase/supabase-js';
 
-const supabase = createClient("https://dzqcqtucdqznjvagxmhj.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR6cWNxdHVjZHF6bmp2YWd4bWhqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA4MDE5OTUsImV4cCI6MjA1NjM3Nzk5NX0.iUdqHG_dvoEfqndwfgqhz1ikik7H7PPk2okKlz8xlb8");
+import { SUPABASE_URL, SUPABASE_SECRET } from '$env/static/private';
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_SECRET);
 
 export async function GET() {
     const { data, error } = await supabase.from('skills').select('*');

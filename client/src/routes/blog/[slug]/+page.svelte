7476e69c8a254e7b1113328783ff517c40f91332
TeejMcSteez <tjhall047@gmail.com>
2025-03-09 23:32:33 -0400

    Removed supabase creds from client code

--- client/src/routes/blog/[slug]/+page.svelte
+++ client/src/routes/blog/[slug]/+page.svelte
@@ -15,9 +15,6 @@
 
     $: slug = $page.params.slug;
 
-    import { createClient } from '@supabase/supabase-js'
-    const supabase = createClient('https://dzqcqtucdqznjvagxmhj.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR6cWNxdHVjZHF6bmp2YWd4bWhqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA4MDE5OTUsImV4cCI6MjA1NjM3Nzk5NX0.iUdqHG_dvoEfqndwfgqhz1ikik7H7PPk2okKlz8xlb8');
-
     /**
      * Fetches blog content and comments from Supabase on component mount.
      * Parses the blog content using marked and sets up comments.
